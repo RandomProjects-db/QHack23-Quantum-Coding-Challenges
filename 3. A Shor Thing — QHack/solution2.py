8b715623eeedfb9f52ec59853d43ab04e4f16208
TradingBot <randomprojects-db@hotmail.com>
2023-02-22 14:46:11 +0200

    new solution 2

---
+++ 3. A Shor Thing — QHack/solution2.py
@@ -0,0 +1,85 @@
+n_qubits = 9
+dev = qml.device("default.qubit", wires=n_qubits)
+error_dict = {0: 'PauliX', 1: 'PauliY', 2: 'PauliZ'}
+
+def error(error_key, qubit):
+    """Defines the error that is induced in the circuit.
+
+    Args:
+        error_key (int): An integer associated to the type of error (Pauli X, Y, or Z)
+        qubit (int): The qubit that the error occurs on.
+    """
+    getattr(qml, error_dict[error_key])(qubit)
+
+@qml.qnode(dev)
+def shor(state, error_key, qubit):
+    """A circuit defining Shor's code for error correction.
+
+    Args:
+        state (list(float)): The quantum state of the first qubit in the circuit.
+        error_key (int): An integer associated to the type of error (Pauli X, Y, or Z)
+        qubit (int): The qubit that the error occurs on.
+
+    Returns:
+        (list(float)): The expectation value of the Pauli Z operator on every qubit.
+    """
+    qml.QubitStateVector(np.array(state), wires=0)
+
+
+    qml.CNOT(wires=[0, 3])
+    qml.CNOT(wires=[0, 6])
+
+    qml.Hadamard(wires=0)
+    qml.Hadamard(wires=3)
+    qml.Hadamard(wires=6)
+
+
+    qml.CNOT(wires=[0, 1])
+    qml.CNOT(wires=[3, 4])
+    qml.CNOT(wires=[6, 7])
+
+    qml.CNOT(wires=[0, 2])
+    qml.CNOT(wires=[3, 5])
+    qml.CNOT(wires=[6, 8])
+
+    error(error_key, qubit)
+
+    qml.CNOT(wires=[0, 1])
+    qml.CNOT(wires=[3, 4])
+    qml.CNOT(wires=[6, 7])
+
+    qml.CNOT(wires=[0, 2])
+    qml.CNOT(wires=[3, 5])
+    qml.CNOT(wires=[6, 8])
+
+    qml.Toffoli(wires=[2, 1, 0])
+    qml.Toffoli(wires=[5, 4, 3])
+    qml.Toffoli(wires=[8, 7, 6])
+
+    qml.Hadamard([0])
+    qml.Hadamard([3])
+    qml.Hadamard([6])
+
+    qml.CNOT(wires=[0, 3])
+    qml.CNOT(wires=[0, 6])
+    qml.Toffoli(wires=[6, 3, 0])
+    
+    exp_vals = [qml.expval(qml.PauliZ(i)) for i in range(n_qubits)]
+
+    return exp_vals
+
+
+# These functions are responsible for testing the solution.
+def run(test_case_input: str) -> str:
+    state, error_key, qubit = json.loads(test_case_input)
+    output = shor(state, error_key, qubit).tolist()
+
+    return str(output)
+
+def check(solution_output: str, expected_output: str) -> None:
+    solution_output = json.loads(solution_output)
+    expected_output = json.loads(expected_output)
+
+    assert np.allclose(solution_output, expected_output, rtol=1e-4)
+
+
